af37ebeed06ef85e2ec286e24ffe67cba0c22604
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-12 11:36:13 -0500

    created dist folder with compressjs files to run on deployed

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const db = require("./models");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
+if (PORT === 8000) {
+  app.use(express.static("public"));
+} else {
+  app.use(express.static("dist"));
+}
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
